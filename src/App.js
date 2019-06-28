c932da3231392733ce9f5534a1fd495611ebeddc
pchr-srf <philipp.christen@srf.ch>
2019-06-28 14:55:26 +0200

    make app into a component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
 }
 
 export default App;
